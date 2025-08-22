75c939620c0783d979cc17a496f32bf18aa6330c
hanaa-elagmi <hanaa.elagmi@gmail.com>
2025-08-22 21:45:17 +0300

    updating portofolio

--- Js/index.js
+++ Js/index.js
@@ -297,3 +297,15 @@ form.addEventListener('submit', (e) => {
         }
     );
 })
+
+
+
+
+
+let n = 24;
+ let l = 0, r = 100, ans = n;
+  while(l <= r) { let mid = Math.floor((l + r) / 2); //50 
+     if(mid * mid <= n) { ans = mid; l = mid + 1; } //
+     else { r = mid - 1; } } //49  24 
+     print(ans);
+
